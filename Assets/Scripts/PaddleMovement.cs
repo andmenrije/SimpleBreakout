34fce165b6b4448a3f581e6ec3b02c8c9e5c8e97
Andrew Menrije <andmenrije@gmail.com>
2021-03-23 00:51:59 -0700

    InitialCheckin

---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+
+    Rigidbody2D _rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+}
